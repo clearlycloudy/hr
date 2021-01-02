a1dd06a1442e2b1f5c0ac876b1fd5e9952fbe7c3
Yuan Liu <yuan6liu@gmail.com>
2021-01-01 17:51:53 -0800

    format spaces

--- med/count_solutions/main.cpp
+++ med/count_solutions/main.cpp
@@ -8,17 +8,17 @@ int64_t solve(int64_t a,int64_t b, int64_t c, int64_t d){
     // check if x = (a +/- sqrt(a^2-4(y^2-by)))/2 \in [1,c]
     int64_t ret = 0;
     for(int64_t y=1;y<=d;++y){
-	auto f = [=](int64_t sign) -> int64_t {
-		     return (a + sign * sqrt(a*a - 4*(y*y-b*y)))/2;
-		 };
-	int64_t choices [] = {f(1), f(-1)};
-	ret = accumulate(choices,
-			 unique(choices,choices+2),
-			 ret,
-			 [=](int acc, int64_t x) -> int {
-			     bool ret = x>=1 && x<=c && x*x-a*x+y*y-b*y==0;
-			     return acc + static_cast<int>(ret);
-			 });
+        auto f = [=](int64_t sign) -> int64_t {
+                     return (a + sign * sqrt(a*a - 4*(y*y-b*y)))/2;
+                 };
+        int64_t choices [] = {f(1), f(-1)};
+        ret = accumulate(choices,
+                         unique(choices,choices+2),
+                         ret,
+                         [=](int acc, int64_t x) -> int {
+                             bool ret = x>=1 && x<=c && x*x-a*x+y*y-b*y==0;
+                             return acc + static_cast<int>(ret);
+                         });
     }
     return ret;
 }
@@ -26,9 +26,9 @@ int64_t solve(int64_t a,int64_t b, int64_t c, int64_t d){
 int main(){
     int q; cin >> q;
     for(int _=0; _<q; ++_){
-	int64_t a,b,c,d;
-	cin>>a>>b>>c>>d;
-	cout << solve(a,b,c,d) << endl;
+        int64_t a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout << solve(a,b,c,d) << endl;
     }
     return 0;
 }
